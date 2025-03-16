0ec1b8f19de66a5a3d3644c37e3b48c9faf66208
tjasagumilar <tjasa.gumilar@student.um.si>
2025-03-16 14:07:01 +0100

    Funkcionalnosti strežnika

--- Server/server.js
+++ Server/server.js
@@ -45,10 +45,10 @@ const server = http.createServer((req, res) => {
       path.join(__dirname, "../Description/odjemalec.html"),
       (err, data) => {
         if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain,  charset=UTF-8" });
+          res.writeHead(500, { "Content-Type": "text/plain, charset=UTF-8" });
           res.end("Napaka pri branju HTML datoteke.");
         } else {
-          res.writeHead(200, { "Content-Type": "text/html,  charset=UTF-8" });
+          res.writeHead(200, { "Content-Type": "text/html, charset=UTF-8" });
           res.end(data);
         }
       }
@@ -58,10 +58,10 @@ const server = http.createServer((req, res) => {
       path.join(__dirname, "../Description/posebnostiOdjemalec.txt"),
       (err, data) => {
         if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain,  charset=UTF-8" });
+          res.writeHead(500, { "Content-Type": "text/plain, charset=UTF-8" });
           res.end("Napaka pri branju tekstovne datoteke.");
         } else {
-          res.writeHead(200, { "Content-Type": "text/plain,  charset=UTF-8" });
+          res.writeHead(200, { "Content-Type": "text/plain, charset=UTF-8" });
           res.end(data);
         }
       }
@@ -72,6 +72,43 @@ const server = http.createServer((req, res) => {
       if (err) {
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("Slika ni najdena.");
+      } else {
+        res.writeHead(200, { "Content-Type": "image/png" });
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/funkcionalnosti-streznika/") {
+    fs.readFile(
+      path.join(__dirname, "../Description/funkcionalnosti-streznika.html"),
+      (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain, charset=UTF-8" });
+          res.end("Napaka pri branju HTML datoteke.");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html, charset=UTF-8" });
+          res.end(data);
+        }
+      }
+    );
+  } else if (req.url === "/posebnosti-streznika/") {
+    fs.readFile(
+      path.join(__dirname, "../Description/posebnosti-streznika.txt"),
+      (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain, charset=UTF-8" });
+          res.end("Napaka pri branju tekstovne datoteke.");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/plain, charset=UTF-8" });
+          res.end(data);
+        }
+      }
+    );
+  } else if (req.url === "/uml-streznika/") {
+    const imgPath = path.join(__dirname, "../Description/diagram.png");
+    fs.readFile(imgPath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Slika UML diagrama strežnika ni najdena.");
       } else {
         res.writeHead(200, { "Content-Type": "image/png" });
         res.end(data);
